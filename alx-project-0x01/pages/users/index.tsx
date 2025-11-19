3da173f09b6d18e52458ce1fb4f1f82e64c6b2bb
ZeeAzeez <azeezahaazeez@gmail.com>
2025-11-19 10:53:55 +0100

    task 2

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Header from "@/components/layout/Header";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <Header />
+      <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold text-white">Users</h1>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default index;
